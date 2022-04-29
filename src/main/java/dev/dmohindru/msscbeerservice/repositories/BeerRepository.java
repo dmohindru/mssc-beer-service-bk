959d4454a27d17fe581c1ccaf32fca4dd67784d0
Dhruv Mohindru <dhruv.mohindru@parcelpoint.com.au>
2022-04-29 17:32:25 +1000

    Added beer object repository. Closes #6

---
+++ src/main/java/dev/dmohindru/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package dev.dmohindru.msscbeerservice.repositories;
+
+import dev.dmohindru.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
